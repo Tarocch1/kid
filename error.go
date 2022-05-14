6b87c4f9fcf8f95c1991c3a3dd7f127728baebae
Tarocch1 <huiben.fang@gmail.com>
2022-05-14 20:26:22 +0800

    feat: default error handler log

--- error.go
+++ error.go
@@ -1,14 +1,18 @@
 package kid
 
 import (
+	"fmt"
 	"net/http"
 )
 
 // ErrorHandlerFunc defines a function to process return errors or panic errors from handlers.
 type ErrorHandlerFunc func(*Ctx, error) error
 
+var errorLogger = NewLogger("HTTP Error")
+
 // DefaultErrorHandler that process return errors or panic errors from handlers.
 var DefaultErrorHandler ErrorHandlerFunc = func(c *Ctx, err error) error {
+	errorLogger.Error(c, fmt.Sprintf("%s %s", c.Method(), c.Url().RequestURI()), nil, err)
 	if e, ok := err.(*Error); ok {
 		return c.Status(e.Status).String(e.Message)
 	} else {
